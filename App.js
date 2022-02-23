d2ed7d081850d96171f978048fcd887338a62f28
siddhi1999 <siddhi.sp1999@gmail.com>
2022-02-23 19:10:39 +0530

    Event Arguments

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ const EventEmitter = require('events');
 const emitter= new EventEmitter();
 
 //Register a Listener. we can also use addListener intead of on
-emitter.on('messageLogged', function () {
-    console.log('Listener called');
+emitter.on('messageLogged', function (arg) {
+    console.log('Listener called', arg);
 });
 
 //Raise an event
-emitter.emit('messageLogged');
+emitter.emit('messageLogged', {id: 1, url: 'http://'});
