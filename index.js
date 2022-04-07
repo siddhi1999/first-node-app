e05bc678660cd0983991ac4cfca3960ba73c124e
siddhi1999 <siddhi.sp1999@gmail.com>
2022-04-07 16:19:55 +0530

    sample.txt added

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const a= {
-    average : (a,b) => { console.log((a+b)/2) },     //this is function
-    precent : (a,b) => {  console.log((a/b)*100) }
-};
+const fs= require("fs");
 
-module.exports = a;
+fs.readFile('sample.txt', "utf-8", (err, data) => {    //write utf-8 in options argument to read the file as a text
+    if(err) {
+        return err;  //or write return 
+    }
+    console.log(data);
+});
+
+console.log("This line will be first");
+
+
+// const a= {
+//     average : (a,b) => { console.log((a+b)/2) },     //this is function
+//     precent : (a,b) => {  console.log((a/b)*100) }
+// };
+
+// module.exports = a;
