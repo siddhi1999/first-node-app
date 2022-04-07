04c87130807a5c2bba0708a005f8e29b04374790
siddhi1999 <siddhi.sp1999@gmail.com>
2022-04-07 09:34:06 +0530

    update in App.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-const Logger= require('./logger');
-const logger= new Logger();
-//Register a Listener. we can also use addListener intead of on
-logger.on('messageLogged', function (arg) {
-    console.log('Listener called', arg);
-});
+const a= require('./index');
+
+a.average(10, 20);
+
+// const Logger= require('./logger');
+// const logger= new Logger();
+// //Register a Listener. we can also use addListener intead of on
+// logger.on('messageLogged', function (arg) {
+//     console.log('Listener called', arg);
+// });
     
-logger.log('message is Here');
-
-
+// logger.log('message is Here');
---
+++ index.js
@@ -0,0 +1,6 @@
+const a= {
+    average : (a,b) => { console.log((a+b)/2) },     //this is function
+    precent : (a,b) => {  console.log((a/b)*100) }
+};
+
+module.exports = a;
